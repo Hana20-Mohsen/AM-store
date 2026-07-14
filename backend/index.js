c44c37e3fc7e139600914f502b13693335ba6b08
Hana20-Mohsen <hana2021mohsen@gmail.com>
2026-07-14 12:41:17 +0300

    first commit

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+import bootstrap from './src/app.controller.js'
+import express from 'express'
+import http from 'http'
+// import initSocket from './src/Socket/index.js'
+const app=express();
+const port =8000;
+
+    const server = http.createServer(app)
+bootstrap(app , express)
+// initSocket(server)
+
+
+server.listen(process.env.PORT || port, () => {
+ console.log("server running")
+})
